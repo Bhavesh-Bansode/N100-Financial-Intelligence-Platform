2904757f0053058869e7710555a46e157087de97
Bhavesh Bansode <bhaveshshashikantbansode@gmail.com>
2026-06-19 22:03:13 +0530

    Modified loader to load supporting files also

--- src/etl/loader.py
+++ src/etl/loader.py
@@ -2,16 +2,27 @@ import pandas as pd
 
 
 def load_excel(path: str) -> pd.DataFrame:
-    """
-    Load Excel file using header row 1.
-    """
 
-    df = pd.read_excel(
+    if any(
+        file_name in path.lower()
+        for file_name in [
+            "sectors",
+            "market_cap",
+            "stock_prices",
+            "peer_groups",
+            "financial_ratios"
+        ]
+    ):
+
+        return pd.read_excel(
+            path,
+            header=0
+        )
+
+    return pd.read_excel(
         path,
         header=1
     )
-
-    return df
 
 
 if __name__ == "__main__":
